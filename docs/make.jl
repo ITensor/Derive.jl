09bbeea591061d98d2cb78847af2cc167283b8e8
mtfishman <mfishman@flatironinstitute.org>
2024-12-05 22:40:47 -0500

    Format

--- docs/make.jl
+++ docs/make.jl
@@ -1,9 +1,7 @@
 using Derive: Derive
 using Documenter: Documenter, DocMeta, deploydocs, makedocs
 
-DocMeta.setdocmeta!(
-  Derive, :DocTestSetup, :(using Derive); recursive=true
-)
+DocMeta.setdocmeta!(Derive, :DocTestSetup, :(using Derive); recursive=true)
 
 include("make_index.jl")
 
@@ -12,13 +10,9 @@ makedocs(;
   authors="ITensor developers <support@itensor.org> and contributors",
   sitename="Derive.jl",
   format=Documenter.HTML(;
-    canonical="https://ITensor.github.io/Derive.jl",
-    edit_link="main",
-    assets=String[],
+    canonical="https://ITensor.github.io/Derive.jl", edit_link="main", assets=String[]
   ),
   pages=["Home" => "index.md"],
 )
 
-deploydocs(;
-  repo="github.com/ITensor/Derive.jl", devbranch="main", push_preview=true
-)
+deploydocs(; repo="github.com/ITensor/Derive.jl", devbranch="main", push_preview=true)
