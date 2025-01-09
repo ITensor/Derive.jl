8d0ecd44ab2eb088f9aeaf1b5fd35a4d551b12a1
Lukas Devos <ldevos98@gmail.com>
2025-01-09 17:10:09 -0500

    Add missing `DefaultArrayInterface` methods

--- src/defaultarrayinterface.jl
+++ src/defaultarrayinterface.jl
@@ -6,3 +6,27 @@ function interface(a::Type{<:AbstractArray})
   parenttype(a) === a && return DefaultArrayInterface()
   return interface(parenttype(a))
 end
+
+@interface ::DefaultArrayInterface function Base.getindex(
+  a::AbstractArray{<:Any,N}, I::Vararg{Int,N}
+) where {N}
+  return Base.getindex(a, I...)
+end
+
+@interface ::DefaultArrayInterface function Base.setindex!(
+  a::AbstractArray{<:Any,N}, value, I::Vararg{Int,N}
+) where {N}
+  return Base.setindex!(a, value, I...)
+end
+
+@interface ::DefaultArrayInterface function Base.map!(
+  f, a_dest::AbstractArray, a_srcs::AbstractArray...
+)
+  return Base.map!(f, a_dest, a_srcs...)
+end
+
+@interface ::DefaultArrayInterface function Base.mapreduce(
+  f, op, as::AbstractArray...; kwargs...
+)
+  return Base.mapreduce(f, op, as...; kwargs...)
+end
